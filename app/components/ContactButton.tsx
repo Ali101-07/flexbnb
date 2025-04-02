59fe7d4427841ae4697e80900ecad9c9b8339153
Ali101-07 <alihassan.iqbal101@gmail.com>
2025-04-02 04:40:23 -0700

    ss

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -1,6 +1,6 @@
 const ContactButton=()=>{
     return(
-       <div className="cursor-pointer  mt-6 py-4 px-6 bg-red-500 hover:bg-red-900 text-white rounded-xl transition">
+       <div className="  mt-6 py-4 px-6 bg-red-500 hover:bg-red-900 text-white rounded-xl transition">
         contact
        </div>
     )
