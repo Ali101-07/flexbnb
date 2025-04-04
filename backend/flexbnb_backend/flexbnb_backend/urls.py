e5e9c5ef75d9939bd8f785cecbef5035df758b15
Ali101-07 <alihassan.iqbal101@gmail.com>
2025-04-05 00:15:06 +0500

    aa

--- backend/flexbnb_backend/flexbnb_backend/urls.py
+++ backend/flexbnb_backend/flexbnb_backend/urls.py
@@ -1,8 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to FlexBnB!")  # Simple homepage response
 
 urlpatterns = [
+    path('', home),  # Route for '/'
     path('admin/', admin.site.urls),
-    path('api/properties/',include('property.urls')),
-] 
+    path('api/properties/', include('property.urls')),
+]
